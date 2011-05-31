8d77af14d0d78ba350103543c5abe29c458cbe75
Sam Vincent <sam.vincent@mac.com>
2011-05-30 20:34:58 -0700

    Background fill supported

--- lib/rqrcode-rails3/renderers/svg.rb
+++ lib/rqrcode-rails3/renderers/svg.rb
@@ -4,15 +4,13 @@ module RQRCode
       class << self
         # Render the SVG from the qrcode string provided from the RQRCode gem
         #   Options:
-        #   offset - Padding around the QrCode e.g. 10
-        #   fill   - Background color e.g "ffffff"
-        #   color  - Foreground color for the code e.g. "000000"
-        #   blank_padding - Only used when using offset and fill options, set to true to leave the padding a blank color. Default is false
+        #   offset - Padding around the QR Code (e.g. 10)
+        #   fill   - Background color (e.g "ffffff" or :white)
+        #   color  - Foreground color for the code (e.g. "000000" or :black)
 
         def render(qrcode, options={})
           offset  = options[:offset].to_i || 0
-          light   = options[:fill]        || "fff"
-          dark    = options[:color]       || "000"
+          color   = options[:color]       || "000"
 
           # height and width dependent on offset and QR complexity
           dimension = (qrcode.module_count*11) + (2*offset)
@@ -28,22 +26,16 @@ module RQRCode
               y = c*11 + offset
               x = r*11 + offset
 
-              next unless qrcode.is_dark(c, r) || options[:fill]
-
-              color = qrcode.is_dark(c, r) ? dark : light
+              next unless qrcode.is_dark(c, r)
               tmp << %{<rect width="11" height="11" x="#{x}" y="#{y}" style="fill:##{color}"/>}
             end 
             result << tmp.join
           end
-
-          # Color background squares as well
-          if options[:offset] && options[:fill] && !options[:blank_padding]
-            result << %{<rect width="#{offset}" height="#{dimension}" x="0" y="0" style="fill:##{light}"/>}
-            result << %{<rect width="#{offset}" height="#{dimension}" x="#{dimension-offset}" y="0" style="fill:##{light}"/>}
-            result << %{<rect width="#{dimension-2*offset}" height="#{offset}" x="#{offset}" y="0" style="fill:##{light}"/>}
-            result << %{<rect width="#{dimension-2*offset}" height="#{offset}" x="#{offset}" y="#{dimension-offset}" style="fill:##{light}"/>}
+          
+          if options[:fill]
+            result.unshift %{<rect width="#{dimension}" height="#{dimension}" x="0" y="0" style="fill:##{options[:fill]}"/>}
           end
-
+          
           svg = [xml_tag, open_tag, result, close_tag].flatten.join("\n")
         end
       end
