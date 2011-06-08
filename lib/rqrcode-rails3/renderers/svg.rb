99a2173c23a5b199d830e1f39035e10ad3ab4b08
Nicholas Phillips <nicholas.d.phillips@gmail.com>
2011-06-08 10:59:54 -0500

    Added ability to change QRCode line width from within the renderer

--- lib/rqrcode-rails3/renderers/svg.rb
+++ lib/rqrcode-rails3/renderers/svg.rb
@@ -5,15 +5,17 @@ module RQRCode
         # Render the SVG from the qrcode string provided from the RQRCode gem
         #   Options:
         #   offset - Padding around the QR Code (e.g. 10)
+        #   width  - Thickness of the lines. Effects the overall size of the QR Code (e.g. 10)
         #   fill   - Background color (e.g "ffffff" or :white)
         #   color  - Foreground color for the code (e.g. "000000" or :black)
 
         def render(qrcode, options={})
           offset  = options[:offset].to_i || 0
+          width   = options[:width].to_i  || 11
           color   = options[:color]       || "000"
 
           # height and width dependent on offset and QR complexity
-          dimension = (qrcode.module_count*11) + (2*offset)
+          dimension = (qrcode.module_count*width) + (2*offset)
 
           xml_tag   = %{<?xml version="1.0" standalone="yes"?>}
           open_tag  = %{<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:ev="http://www.w3.org/2001/xml-events" width="#{dimension}" height="#{dimension}">}
@@ -23,11 +25,11 @@ module RQRCode
           qrcode.modules.each_index do |c|
             tmp = []
             qrcode.modules.each_index do |r|
-              y = c*11 + offset
-              x = r*11 + offset
+              y = c*width + offset
+              x = r*width + offset
 
               next unless qrcode.is_dark(c, r)
-              tmp << %{<rect width="11" height="11" x="#{x}" y="#{y}" style="fill:##{color}"/>}
+              tmp << %{<rect width="#{width}" height="#{width}" x="#{x}" y="#{y}" style="fill:##{color}"/>}
             end 
             result << tmp.join
           end
